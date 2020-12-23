0e57c06c8b243bd8093a713fb6080e1ef5b36f03
Maks Klimashevskiy <38065721+saegets@users.noreply.github.com>
2020-12-23 23:41:49 +0200

    Add files via upload

---
+++ 6.3 сп1/6.3 сп1/6.3 сп1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void Create(int* a, const int size)
+{
+    for (int i = 0; i < size; i++)
+        a[i] = rand() % 26;
+}
+
+void suma(int* a, const int size, int& S)
+{
+    for (int i = 0; i < size; i++)
+        if (a[i] % 2 == 0)
+        {
+            S += a[i];
+        }
+}
+
+void print(int* a, const int size, int& S)
+{
+    cout << setw(4) << "a[10] = {";
+    for (int i = 0; i < size; i++)
+    {
+        cout << setw(4) << a[i] << ", ";
+    }
+    cout << "}" << endl;
+
+    cout << "Suma =" << S << endl;
+    cout << endl;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    const int n = 10;
+    int a[n];
+    Create(a, n);
+    int S = 0; 
+    suma(a, n, S);
+    print(a, n, S);
+
+    return 0;
+}
---
+++ 6.3 сп2/6.3 сп2/6.3 сп2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+void Create(int* a, const int size, int i)
+{
+    a[i] = rand() % 26;
+
+    if (i < size - 1)
+        Create(a, size, i + 1);
+}
+
+void Print(int* a, const int size, int i, int& S)
+{
+    if (i == 0)
+        cout << "a[10]={ ";
+    cout << setw(4) << a[i] << ", ";
+    if (i < size - 1)
+        Print(a, size, i + 1, S);
+    else
+        cout << " }" << '\n' << "suma = " << S << endl;
+}
+
+int Sum(int* a, const int size, int i, int& S)
+{
+    if (a[i] % 2 == 0)
+        S += a[i];
+    if (i < size - 1)
+        return Sum(a, size, i + 1, S);
+    else
+        return S;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    const int n = 10;
+    int a[n];
+    int S = 0;
+    Create(a, n, 0);
+    Sum(a, n, 0, S);
+    Print(a, n, 0, S);
+
+    return 0;
+}
